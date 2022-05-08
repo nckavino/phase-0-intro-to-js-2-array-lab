47056c926fb36252107b91a893e2a4849041ed18
Nicholas Avino <nckavino0903@gmail.com>
2022-05-08 13:35:38 -0400

    commit tree ae0f74d9e537d513583081c904f70b8266dce66f

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(string) {
+    cats.push(string);
+}
+
+function destructivelyPrependCat(string) {
+    cats.unshift(string);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const appendCat = [...cats, name];
+    return appendCat;
+}
+
+function prependCat(name) {
+    const prependCat =[name, ...cats];
+    return prependCat;
+}
+
+function removeLastCat() {
+    const removeLastCat = cats.slice(0, cats.length -1);
+    return removeLastCat;
+}
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat; 
+}
